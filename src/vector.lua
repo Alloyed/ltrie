529eda883d4ad24133c7da9605296936ceb441ae
Kyle McLamb <kjmclamb@gmail.com>
2015-04-24 07:56:11 -0400

    Add documentation, ipairs

--- src/vector.lua
+++ src/vector.lua
@@ -1,9 +1,23 @@
-local function try(f, ...)
-	local ok, err = pcall(f, ...)
+--- A persistent vector implementation, modeled after Clojure's
+-- PersistentVector. A good blogpost explaining the structure:
+--
+-- <http://hypirion.com/musings/understanding-persistent-vector-pt-1>
+--
+-- To match Lua's index-by-one semantics I had to introduce an offset or two.
+-- Every line I've done that on is marked with a +1 or -1 comment.
+-- @module Vector
+--
+local function try(...)
+	local ok, err = pcall(...)
 	if not ok then return nil end
 	return err
 end
-local b = bit32 or try(require, 'bit') or error("No bitwise lib found")
+local b = bit32 or try(require, 'bit') or error("No bitop lib found")
+
+local BITS  = 5
+local WIDTH = 32
+local MASK  = 31
+
 local Vector = {}
 
 local mt = {
@@ -20,11 +34,14 @@ end
 
 local EMPTY = Vec {
 	count = 0,
-	shift = 5,
-	root = {},
-	tail = {},
+	shift = BITS,
+	root  = {},
+	tail  = {},
 }
 
+--- Creates a vector containing the elements provided by the given iterator.
+-- @param ... the iterator
+-- @usage Vector.from(ipairs {1, 2, 3, 4, 5})
 function Vector.from(...)
 	local r = EMPTY
 	for _, v in ... do
@@ -33,14 +50,25 @@ function Vector.from(...)
 	return r
 end
 
+--- Creates a vector containing the arguments.
+-- @param ... the arguments to include
+-- @usage Vector.of(1, 2, 3, 4, 5)
+function Vector.of(...)
+	local r =  EMPTY
+	for i=1, select('#', ...) do
+		r = r:conj(selec(i, ...))
+	end
+	return r
+end
+
+--- @type Vector
+
+--- Returns the number of elements contained in the vector.
+-- In lua 5.2 and up, `#vector` can be used as a shortcut.
 function Vector:len()
 	return self.count
 end
 mt.__len = Vector.len
-
-local BITS  = 5
-local WIDTH = 32
-local MASK  = 31
 
 local function mask(i)
 	return b.band(i, MASK)
@@ -50,14 +78,16 @@ local function tailoff(trie)
 	if trie.count < WIDTH then
 		return 0
 	else
-		-- trie.count - 1 is original
-		return b.lshift(b.rshift(trie.count - 1, 5), 5)
+		local last_idx = trie.count - 1
+		return b.lshift(b.rshift(last_idx, BITS), BITS)
 	end
 end
 
+local OOB = {}
+
 local function arrayFor(trie, idx)
 	if not(idx >= 0 and idx < trie.count) then
-		error("Index out of bounds")
+		return OOB
 	end
 
 	if idx >= tailoff(trie) then
@@ -67,18 +97,36 @@ local function arrayFor(trie, idx)
 	local node = trie.root
 	local level = trie.shift
 	while level > 0 do
-		local newidx = mask(b.rshift(idx, level)) + 1
+		local newidx = mask(b.rshift(idx, level)) + 1 -- +1
 		node = node[newidx]
 		level = level - BITS
 	end
 	return node
 end
 
+--- Returns the value stored at `idx`, or `nil` otherwise.
+-- @tparam int idx the index
 function Vector:get(idx)
-	idx = idx - 1
+	idx = idx - 1 -- -1
 	local node = arrayFor(self, idx)
-	return node[mask(idx) + 1]
+	if node == OOB then return nil end
+	return node[mask(idx) + 1] -- +1
 end
+mt.__index = Vector.get
+
+
+local function iter(param, state)
+	local vec, idx = param, state
+	idx = idx + 1
+	local val = vec:get(idx)
+	if val then
+		return idx, val
+	end
+end
+function Vector:ipairs()
+	return iter, self, 0
+end
+mt.__ipairs = Vector.ipairs
 
 local function newPath(level, node)
 	if level == 0 then
@@ -94,7 +142,7 @@ local function copy(tbl)
 end
 
 local function pushTail(self, level, parent, tailNode)
-	local subidx = mask(b.rshift(self.count - 1, level)) + 1
+	local subidx = mask(b.rshift(self.count - 1, level)) + 1 -- +1
 	local r = copy(parent)
 
 	local nodeToInsert
@@ -110,6 +158,8 @@ local function pushTail(self, level, parent, tailNode)
 	return r
 end
 
+--- Returns a new vector with val appended to the end.
+-- @param val the value to append
 function Vector:conj(val)
 	local idx = self.count
 	-- Is there room in the tail?
@@ -149,16 +199,19 @@ end
 local function doAssoc(level, node, idx, val)
 	local r = copy(node)
 	if level == 0 then
-		r[mask(idx) + 1] = val
+		r[mask(idx) + 1] = val -- +1
 	else
-		local subidx = mask(b.rshift(idx, level)) + 1
+		local subidx = mask(b.rshift(idx, level)) + 1 -- +1
 		r[subidx] = doAssoc(level - BITS, node[subidx], idx, val)
 	end
 	return r
 end
 
+--- Returns a new vector such that `vector:get(idx) == val`
+-- @tparam int idx the index to set.
+-- @param val the value to set
 function Vector:assoc(idx, val)
-	idx = idx - 1
+	idx = idx - 1 -- -1
 	if idx == self.count then
 		return self:conj(va)
 	end
@@ -167,7 +220,7 @@ function Vector:assoc(idx, val)
 	end
 	if idx >= tailoff(self) then
 		local newTail = copy(self.tail)
-		newTail[mask(idx) + 1] = val
+		newTail[mask(idx) + 1] = val -- +1
 		return Vec {
 			count = self.count,
 			shift = self.shift,
